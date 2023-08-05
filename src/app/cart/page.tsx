96dca1046565b2d6095a2d9f6eec87cfab4c520e
Pilag6 <pilag6@gmail.com>
2023-08-05 11:54:27 +0200

    Add favicon

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,8 +4,10 @@ import React from "react";
 const CartPage = () => {
     return (
         <div className="h-[calc(100vh-3rem)] md:h-[calc(100vh-6rem)] flex flex-col text-red-500 lg:flex-row">
+          
             {/* PRODUCTS CONTAINER */}
-            <div className="h-1/2 p-4 flex flex-col justify-between overflow-y-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
+            <div className="h-1/2 p-4 flex flex-col justify-center overflow-y-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
+
                 {/* SINGLE ITEM 1*/}
                 <div className="flex items-center justify-between mb-4 gap-8">
                     <Image
@@ -23,7 +25,7 @@ const CartPage = () => {
                             <span>Large</span>
                         </div>
                         <div className="flex gap-8">
-                            <h2 className="font-bold">$79.90</h2>
+                            <h2 className="font-bold">$14.90</h2>
                             <span className="cursor-pointer">X</span>
                         </div>
                     </div>
@@ -43,10 +45,10 @@ const CartPage = () => {
                             <h1 className="uppercase text-xl font-bold">
                                 bacon deluxe
                             </h1>
-                            <span>Medium</span>
+                            <span>Extra BBQ sauce</span>
                         </div>
                         <div className="flex gap-8">
-                            <h2 className="font-bold">$59.90</h2>
+                            <h2 className="font-bold">$19.90</h2>
                             <span className="cursor-pointer">X</span>
                         </div>
                     </div>
@@ -55,7 +57,7 @@ const CartPage = () => {
                 {/* SINGLE ITEM 3*/}
                 <div className="flex items-center justify-between mb-4 gap-8">
                     <Image
-                        src="/temporary/p6.png"
+                        src="/temporary/p4.png"
                         alt=""
                         width={100}
                         height={100}
@@ -64,12 +66,12 @@ const CartPage = () => {
                     <div className="flex gap-8 justify-between items-start w-full">
                         <div className="flex flex-col items-start justify-center">
                             <h1 className="uppercase text-xl font-bold">
-                                mediterranean delight
+                                spicy arrabiata
                             </h1>
-                            <span>Large</span>
+                            <span>Extra Parmessan</span>
                         </div>
                         <div className="flex gap-8">
-                            <h2 className="font-bold">$79.90</h2>
+                            <h2 className="font-bold">$12.00</h2>
                             <span className="cursor-pointer">X</span>
                         </div>
                     </div>
@@ -77,10 +79,10 @@ const CartPage = () => {
             </div>
 
             {/* PAYMENT CONTAINER */}
-            <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
+            <div className="h-1/2 p-4 bg-green-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
                 <div className="flex justify-between">
                     <span className="">Subtotal (3 items)</span>
-                    <span className="">$81.70</span>
+                    <span className="">$46.80</span>
                 </div>
                 <div className="flex justify-between">
                     <span className="">Service Cost</span>
@@ -92,10 +94,10 @@ const CartPage = () => {
                 </div>
                 <hr className="my-2" />
                 <div className="flex justify-between">
-                    <span className="">TOTAL(INCL. VAT)</span>
-                    <span className="font-bold">$81.70</span>
+                    <span className="">TOTAL <small>(INCL. VAT)</small></span>
+                    <span className="font-bold">$46.80</span>
                 </div>
-                <button className="bg-red-500 text-white p-3 rounded-md w-1/2 self-end">
+                <button className="bg-red-500 text-white p-3 rounded-md w-1/2 lg:w-full self-end">
                     CHECKOUT
                 </button>
             </div>
